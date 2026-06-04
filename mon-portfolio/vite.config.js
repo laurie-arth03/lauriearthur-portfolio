a545b136f74fcc84c1c27a83af00bb295fa3e1ee
Laurie <lauriarth@gmail.com>
2026-06-04 14:45:07 +0300

    Mise à jour

--- mon-portfolio/vite.config.js
+++ mon-portfolio/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // On utilise le plugin standard ici
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+   plugins: [react()],
+  base: '/budget_companion_bank/',
 })
